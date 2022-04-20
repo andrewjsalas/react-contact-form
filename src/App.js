8ec2057a873a2cb38fb5a55ef2cfe5e2af586c2f
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-04-20 12:16:14 -0700

    Create basic form
    Created the basic structure of the contact form and set state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer>
+      <PageSubHeader>Get In Touch</PageSubHeader>
+      <PageHeader>Contact</PageHeader>
+
+      <ContactCard>
+
+        <InputFields>
+          <Label htmlFor='name'>
+            <Input 
+              type='text'
+              name='name' 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            Name
+          </Label>        
+          <Label htmlFor='email'>
+            <Input 
+              type='text'
+              email='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            Email
+          </Label>      
+          <Label htmlFor='text-area'>
+            <TextArea
+              type='text'
+              message='message'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            Message
+          </Label>
+        </InputFields>
+
+        <Button>Send</Button>
+      </ContactCard>
+
+    </PageContainer>
   );
 }
+
+const PageContainer = styled.div`
+
+`
+
+const PageHeader = styled.h1`
+
+`
+
+const PageSubHeader = styled.h4`
+
+`
+
+const ContactCard = styled.div`
+
+`
+
+const InputFields = styled.div`
+
+`
+
+const Label = styled.h3`
+
+`
+
+const Input = styled.input`
+
+`
+
+const TextArea = styled.textarea`
+
+`
+
+const Button = styled.button`
+`
 
 export default App;
